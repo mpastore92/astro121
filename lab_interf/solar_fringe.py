64b05f7864dbb9c669471d7f357a95d0858ad3e3
Vikram Iyer <viyers1@gmail.com>
2014-03-15 06:06:01 -0700

    Finished basic functionality to record data and re-point the telescope periodically. Both the telescope controller and the record data function run in independent threads as daemons. Added: Argument parser Real time plot option

--- lab_interf/solar_fringe.py
+++ lab_interf/solar_fringe.py
@@ -2,72 +2,77 @@
 import ephem
 import numpy as np
 import time
-from threading import Thread
+import threading
 import radiolab
-print 'imports ok'
+import argparse
 
-## Create an sun and observer objects
-#sun = ephem.Sun()
-#obs = ephem.Observer()
-#
-## Set lat and long, date
-#obs.lat = 37.8732 * np.pi/180
-#obs.long = -122.2573 * np.pi/180 
-#obs.date = ephem.now()
-#
-## Compute sun position
-#sun.compute(obs)
-
-def alt_limit(alt):
-    ''' Limit  coordinates to  max az/alt'''
-    if alt > 87:
+def get_alt():
+    """ Returns altitude within the acceptable range.
+    Returns:
+        float: altitude value of sun limited to the range [15,87] 
+    """
+    if sun.alt > 87:
         return 87
-    elif alt < 15:
+    elif sun.alt < 15:
         return 15
+    else:
+        return sun.alt
 
-def record_data(n=30):
-    '''Save data every n seconds.'''
-    radiolab.recordDVM(filename='sun_'+time.strftime("%m-%d-%Y_%H%M%S")+'.npz', sun=True, recordLength=n)
-    
-def controller(n=30):
-    '''Update  every n seconds.'''
-    radiolab.pntTo(sun.az, alt_limit(sun.alt))
-    time.sleep(n)
-def test():
-    #print(str(obs.sidereal_time()))
-    print('test')
-    time.sleep(1)
+def controller(t=30.0):
+    """Re-point telescope every t seconds.
+    Args:
+        t (float, optional): number of seconds between updates.  Defaults to 30.0 seconds.
+    """
+    while(True):
+        radiolab.pntTo(sun.az, get_alt())
+        time.sleep(n)
+
+def record_data(t):
+    """Record data for t seconds
+    Args:
+        t (float): number of seconds to record 
+    """
+    radiolab.recordDVM(filename='sun_'+time.strftime("%m-%d-%Y_%H%M%S")+'.npz', sun=True, recordLength=1)
 
 if __name__ == "__main__":
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Record solar fringe  data using the interferometer.')
+    parser.add_argument('t', metavar='T', type=int, nargs=1, help='time to record')
+    parser.add_argument('-p', action='store_true', default=False, help='show real time plot')
+    args = parser.parse_args()  
     
-    #radiolab.pntHome()
-    controllerd = Thread(target = test)
-    print 'Starting thread'
-    controllerd.start()
-    time.sleep(5)
-    controllerd.join()
-    print 'Stopping thread'
-'''
-    controllerd.daemon = True
+    # Create an sun and observer objects
+    sun = ephem.Sun()
+    obs = ephem.Observer()
+    
+    # Set lat and long, date
+    obs.lat = 37.8732 * np.pi/180
+    obs.long = -122.2573 * np.pi/180 
+    obs.date = ephem.now()
+    
 
-    datad = Thread(target = radiolab.recordDVM, args=(filename='sun_'+time.strftime("%m-%d-%Y_%H%M%S")+'.npz', sun=True, recordLength=n)
-))
+    # Compute sun position
+    sun.compute(obs)
+
+    # Start telescopes at home position
+    radiolab.pntHome()
+    
+    # Create a thread that periodically re-points the telescope
+    controllerd = threading.Thread(target = test, (args.t[0],))
+
+    # Create to log data 
+    datad = threading.Thread(target = record_data, (args.t[0]+10,))
+
+    # Set threads as a daemons, will close automatically
+    controllerd.daemon = True
     datad.daemon = True
 
+    # Start controller
     controllerd.start()
+
+    # Wait 5 seconds for telescopes to move and start recording  
     time.sleep(5)
-    #TODO: check time required to start
     datad.start()
-    #
-    print "ra: "+str(sun.ra)
-    print "dec: "+str(sun.dec)
-'''
-
-# Set to alt az position
-
-#recordDVM
-#getDVMData
-#getLST
-
-#showplot
-# note: sun should be decent looking sinusoid
+    
+    if args.p:
+        radiolab.plotMeNow()
