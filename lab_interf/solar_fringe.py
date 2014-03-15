4036bdfff254c56b6662fb7a4a9ca07a7e101763
Vikram Iyer <viyers1@gmail.com>
2014-03-15 08:10:20 -0700

    Added logging functionality Logs are stored in the logs directory with the same naming convention as data files: sun_DD-MM-YYYY_HHMMSS

--- lab_interf/solar_fringe.py
+++ lab_interf/solar_fringe.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
-import ephem
+import logging
+import os
 import numpy as np
 import time
 import threading
-import radiolab
 import argparse
+
+import ephem
+import radiolab
 
 def get_alt():
     """ Returns altitude within the acceptable range.
@@ -12,8 +15,10 @@ def get_alt():
         float: altitude value of sun limited to the range [15,87] 
     """
     if sun.alt > 87:
+        logger.info('Detected alt above 87: %s', str(alt))
         return 87
     elif sun.alt < 15:
+        logger.info('Detected alt below 15: %s', str(alt))
         return 15
     else:
         return sun.alt
@@ -24,24 +29,44 @@ def controller(t=30.0):
         t (float, optional): number of seconds between updates.  Defaults to 30.0 seconds.
     """
     while(True):
+        logger.debug('Move to telescope to (alt, az): (%s,%s)', str(get_alt), str(az))
         radiolab.pntTo(sun.az, get_alt())
-        time.sleep(n)
+        time.sleep(t)
 
 def record_data(t):
     """Record data for t seconds
     Args:
         t (float): number of seconds to record 
     """
-    radiolab.recordDVM(filename='sun_'+time.strftime("%m-%d-%Y_%H%M%S")+'.npz', sun=True, recordLength=1)
+    radiolab.recordDVM(filename='logs/'+OBSERVATION, sun=True, recordLength=t)
 
-if __name__ == "__main__":
+def main():
     # Parse arguments
     parser = argparse.ArgumentParser(description='Record solar fringe  data using the interferometer.')
     parser.add_argument('t', metavar='T', type=int, nargs=1, help='time to record')
     parser.add_argument('-p', action='store_true', default=False, help='show real time plot')
     args = parser.parse_args()  
     
-    # Create an sun and observer objects
+    # Create standard file name for log and data files
+    OBSERVATION = 'sun_'+time.strftime("%m-%d-%Y_%H%M%S")
+
+    # Set up logger
+    FORMAT = '%(asctime)-15s - %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger('interferometer')
+    fh = logging.FileHandler(os.getcwd()+'/logs/'+OBSERVATION+'.log')
+    fh.setLevel(logging.INFO)
+  
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+  
+    formatter = logging.Formatter(FORMAT)
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    #Create an sun and observer objects
     sun = ephem.Sun()
     obs = ephem.Observer()
     
@@ -50,11 +75,17 @@ if __name__ == "__main__":
     obs.long = -122.2573 * np.pi/180 
     obs.date = ephem.now()
     
+    logger.debug('Observer Lat: '+str(obs.lat))
+    logger.debug('Observer Long: '+str(obs.lat))
+    logger.debug('Observer Date: '+str(obs.date))
 
     # Compute sun position
     sun.compute(obs)
+    logger.debug('Sun alt: %d', sun.alt)
+    logger.debug('Sun az: %d', sun.az))
 
     # Start telescopes at home position
+    logger.info('Set to home position')
     radiolab.pntHome()
     
     # Create a thread that periodically re-points the telescope
@@ -68,11 +99,17 @@ if __name__ == "__main__":
     datad.daemon = True
 
     # Start controller
+    logger.info('Start position controller')
     controllerd.start()
 
     # Wait 5 seconds for telescopes to move and start recording  
     time.sleep(5)
+    logger.info('Start logging data')
     datad.start()
     
     if args.p:
+        logger.info('Start plot')
         radiolab.plotMeNow()
+
+if __name__ == "__main__":
+   main() 
