373c3b6ae33576272aeb18a6264f3042c68b5d8a
Vikram Iyer <viyers1@gmail.com>
2014-04-01 18:56:16 -0700

    Added simple script to plot data.

---
+++ lab_interf/plot.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+
+def main():
+    """Plots data logs/sun_DD-MM-YY_HHMMSS.
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Plot a file in a numpy readable format.')
+    parser.add_argument('filename', help='file name of data to plot')
+    parser.add_argument('key', default='volts', help='key to plot from npz files')
+    parser.add_argument('-f', '--fft', action='store_true',
+            default=False,help='plot fft of data')
+
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='print  voltage measurements')
+    args = parser.parse_args()
+
+    try:
+        data = np.load(args.filename)
+        plt.plot(data[args.key])
+        plt.show()
+    except Exception, e:
+        print('could not load '+args.filename)
+
+
+if __name__ == "__main__":
+    main()
